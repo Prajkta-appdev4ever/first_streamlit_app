aff46f36d3e019e1d4cd4d3c6fddbc010ec49e9b
Prajkta-appdev4ever <119843991+Prajkta-appdev4ever@users.noreply.github.com>
2022-12-05 14:32:00 +0530

    Update stramlit_app.py
    added breakfast menu header and 3 items

--- stramlit_app.py
+++ stramlit_app.py
@@ -4,8 +4,10 @@ streamlit.title('My Parents New Healty Diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text('Omega 3 & Blueberry oatmeal')
+streamlit.text('🥣 Omega 3 & Blueberry oatmeal')
 
-streamlit.text('Kale , Spinach & Rocket Smoothie')
+streamlit.text('🥗 Kale , Spinach & Rocket Smoothie')
 
-streamlit.text('Hard Boiled Free-Range Egg')
+streamlit.text('🐔 Hard Boiled Free-Range Egg')
+
+streamlit.text('🥑🍞 Avocado Toast')
