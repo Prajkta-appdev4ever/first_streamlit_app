9fc2b2f967c8d9fec21fadf1a3d390c5654d23b0
Prajkta-appdev4ever <119843991+Prajkta-appdev4ever@users.noreply.github.com>
2022-12-12 16:57:31 +0530

    Update and rename stramlit_app.py to streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,19 +8,17 @@ import snowflake.connector
 
 from urllib.error import URLError
 
-streamlit.title('My Moms New Healty Diner')
+#streamlit.title('My Moms New Healty Diner')
 
-streamlit.header('Breakfast Menu')
+#streamlit.header('Breakfast Menu')
 
-streamlit.text('🥣 Omega 3 & Blueberry oatmeal')
+#streamlit.text('🥣 Omega 3 & Blueberry oatmeal')
 
-streamlit.text('🥗 Kale , Spinach & Rocket Smoothie')
+#streamlit.text('🐔 Hard Boiled Free-Range Egg')
 
-streamlit.text('🐔 Hard Boiled Free-Range Egg')
+#streamlit.text('🥑🍞 Avocado Toast')
 
-streamlit.text('🥑🍞 Avocado Toast')
-
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+#streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 #import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
