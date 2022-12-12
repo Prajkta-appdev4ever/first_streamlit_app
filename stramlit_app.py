3f82da266cc48cf50adf9fb97e26b2e4c4ed8832
Prajkta-appdev4ever <119843991+Prajkta-appdev4ever@users.noreply.github.com>
2022-12-12 14:57:56 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -52,7 +52,7 @@ def get_fruityvice_data(this_fruit_choice):
     # write your own comment - what does this do?
     return fruityvice_normalized
   
-   streamlit.header("Fruityvice Fruit Advice!")
+streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
