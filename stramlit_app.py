d8eeb3ad8a40d577d4808780f828a06713d33648
Prajkta-appdev4ever <119843991+Prajkta-appdev4ever@users.noreply.github.com>
2022-12-12 14:56:10 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -37,18 +37,32 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
-streamlit.header("Fruityvice Fruit Advice!")
+#streamlit.header("Fruityvice Fruit Advice!")
+#try:
+#  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  #if not fruit_choice:
+   #  streamlit.error("please select a fruit to get information.")
+  #else:
+    #import requests
+    # Create the repeatable code block (called a function)
+ def get_fruityvice_data(this_fruit_choice):   
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+    # write your own comment -what does the next line do? 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # write your own comment - what does this do?
+    return fruityvice_normalized
+  
+  streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
      streamlit.error("please select a fruit to get information.")
   else:
-    #import requests
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
-    # write your own comment -what does the next line do? 
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    # write your own comment - what does this do?
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe = (back_from_function)
+  
+  
+  
       
 #streamlit.write('The user entered ', fruit_choice)
 
