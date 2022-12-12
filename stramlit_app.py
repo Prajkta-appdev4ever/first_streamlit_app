a40f0b6e06d0827a64e24f481358f7b649cd0f63
Prajkta-appdev4ever <119843991+Prajkta-appdev4ever@users.noreply.github.com>
2022-12-12 16:48:16 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -82,8 +82,8 @@ def get_fruit_load_list():
     return my_cur.fetchall()
   
 #Add button to load fruit
-if streamlit.button('Get Fruit Load List'):
-  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+
+ 
   my_data_rows = get_fruit_load_list()
   my_cnx.close()
   streamlit.dataframe(my_data_rows)  
@@ -96,7 +96,18 @@ if streamlit.button('Get Fruit Load List'):
 #streamlit.header("the fruit load list contains:")
 #streamlit.dataframe(my_data_rows)
 
-fruit_choice = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.header("thanks for adding jackfruit")
 
-my_cur.execute("insert into fruit_load_list values ('from stremlit')");
+streamlit.write("thanks for adding' , add_my_fruit")
+
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values ('from stremlit')");
+    return "Thanks for adding" + new_fruit
+  
+ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+#Add button to load fruit
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = get_fruityvice_data(fruit_choice)
+  streamlit.text = (back_from_function)
+
